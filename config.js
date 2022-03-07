7f739419e6ff717fe14d4f48ada8c2de80cebb09
Lyonict <charles42.h@gmail.com>
2022-03-07 12:05:33 +0100

    fallback before FB tests

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAi0ksUNi2lRTEMLssn4pkI8fGx1Giw65Y",
+    authDomain: "https://linx-8958c-default-rtdb.europe-west1.firebasedatabase.app/",
+    authDomain: "linx-8958c.firebaseapp.com",
+    projectId: "linx-8958c",
+    storageBucket: "linx-8958c.appspot.com",
+    messagingSenderId: "509477785274",
+    appId: "1:509477785274:web:9ecb2324d05fa730a23c83"
+  };
